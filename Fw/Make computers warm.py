6872aa22b8bede62248c6e95a4c023be3f735596
RyanFeng825 <zheruifeng@gmail.com>
2020-06-21 12:19:29 +0800

    Create Make computers warm.py

---
+++ Fw/Make computers warm.py
@@ -0,0 +1,12 @@
+while True:
+  A = raw_input('what do you want:')
+  if A == 'ok':
+     B = 0
+  else:
+     B = 1
+  if B == 1:
+    pass
+  else:
+    break
+print('thank you for your using')
+  
