2efda043996fa68bab775c29d8c4acd10018337f
RyanFeng825 <zheruifeng@gmail.com>
2020-06-21 12:19:41 +0800

    Create Ask yourself.py

---
+++ Fw/Ask yourself.py
@@ -0,0 +1,9 @@
+while True:
+  reply = raw_input('question or answer:')
+  if reply == 'ok':
+      break
+  else:
+      a = reply
+      b = 'Why'+a
+      print (b)
+print('Thank you for your using')
